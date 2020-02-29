f2e01ba6281618eaa5ceb8e4a9c5c65657998b02
David Sevilla Martin <me@datitisev.me>
2020-02-29 08:35:48 -0500

    Modify foreign keys to allow for NULL added user IDs and delete notes when user is deleted

---
+++ migrations/2020_02_29_000001_modify_users_notes_foreign_keys.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('users_notes', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['added_by_user_id']);
+        });
+
+        $schema->table('users_notes', function(Blueprint $table) {
+            $table->integer('added_by_user_id')->unsigned()->nullable()->change();
+        });
+
+        $schema->table('users_notes', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('added_by_user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        // changes should be kept
+    }
+];
