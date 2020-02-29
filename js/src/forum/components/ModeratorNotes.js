2ce709f4f63e0e781f0d24ef30ace478ce8a950a
Ian Morland <ianm@giffgaff.co.uk>
2020-02-29 09:11:00 +0000

    Use -toolbar class for action cta

--- js/src/forum/components/ModeratorNotes.js
+++ js/src/forum/components/ModeratorNotes.js
@@ -4,79 +4,92 @@ import LoadingIndicator from 'flarum/components/LoadingIndicator';
 import NoteListItem from './NoteListItem';
 import Button from 'flarum/components/Button';
 import NoteCreate from './ModeratorNotesCreate';
+import listItems from 'flarum/helpers/listItems';
+import ItemList from 'flarum/utils/ItemList';
 
 export default class ModeratorNotes extends Component {
-  init() {
-    super.init();
-    this.loading = true;
-    this.notes = [];
-    this.refresh();
-  }
-
-  view() {
-
-    let loading;
-
-    if (this.loading) {
-      loading = LoadingIndicator.component();
+    init() {
+        super.init();
+        this.loading = true;
+        this.notes = [];
+        this.refresh();
     }
 
-    const { user } = this.props.params;
+    view() {
 
-    return (
-      <div className="DiscussionList">
-        <h1 className="DiscussionList-notes">{app.translator.trans('fof-moderator-notes.forum.user.notes')}</h1>
-        {user.canCreateModeratorNotes() ? (
-        <div>
-          <Button 
-            className="Button Button--primary Button--block" 
-            onclick={this.handleOnClickCreate.bind(this)}>
-            {app.translator.trans('fof-moderator-notes.forum.moderatorNotes.add_button')}
-          </Button>
-        </div>) : (<div/>)}
-        <p></p>
-        <ul className="DiscussionList-discussions">
-          {this.notes.map(note => {
-            return (
-              <li key={note.id()} data-id={note.id()}>
-                {NoteListItem.component({ note })}
-              </li>
-            );
-          })}
-          {!this.loading
-            && this.notes.length === 0
-            && <label>{app.translator.trans('fof-moderator-notes.forum.moderatorNotes.noNotes')}</label>}
-        </ul>
-        <div className="DiscussionList-loadMore">
-          {loading}
-        </div>
-      </div>
-    );
-  }
+        let loading;
 
-  parseResults(results) {
-    [].push.apply(this.notes, results);
-    this.loading = false;
-    m.lazyRedraw();
+        if (this.loading) {
+            loading = LoadingIndicator.component();
+        }
 
-    return results;
-  }
 
-  refresh() {
-    return app.store.find('notes', this.props.params.user.id()).then(
-      results => {
-        this.notes = [];
-        this.parseResults(results);
-      },
-      () => {
+
+        return (
+            <div className="DiscussionList">
+                <h1 className="DiscussionList-notes">{app.translator.trans('fof-moderator-notes.forum.user.notes')}</h1>
+                <div class="ModeratorNotes-toolbar">
+                    <ul className="ModeratorNotes-toolbar-action">{listItems(this.actionItems().toArray())}</ul>
+                </div>
+                <ul className="DiscussionList-discussions">
+                    {this.notes.map(note => {
+                        return (
+                            <li key={note.id()} data-id={note.id()}>
+                                {NoteListItem.component({ note })}
+                            </li>
+                        );
+                    })}
+                    {!this.loading
+                        && this.notes.length === 0
+                        && <label>{app.translator.trans('fof-moderator-notes.forum.moderatorNotes.noNotes')}</label>}
+                </ul>
+                <div className="DiscussionList-loadMore">
+                    {loading}
+                </div>
+            </div>
+        );
+    }
+
+    actionItems() {
+        const { user } = this.props.params;
+        const items = new ItemList();
+        const canCreateNote = user.canCreateModeratorNotes();
+
+        items.add('create_note',
+            Button.component({
+                children: app.translator.trans('fof-moderator-notes.forum.moderatorNotes.add_button'),
+                className: "Button Button--primary",
+                onclick: this.handleOnClickCreate.bind(this),
+                disabled: !canCreateNote
+            })
+        );
+
+        return items;
+    }
+
+    parseResults(results) {
+        [].push.apply(this.notes, results);
         this.loading = false;
-        m.redraw();
-      }
-    )
-  }
+        m.lazyRedraw();
 
-  handleOnClickCreate(e) {
-    e.preventDefault();
-    app.modal.show(new NoteCreate(this.props.params));
-  }
+        return results;
+    }
+
+    refresh() {
+        return app.store.find('notes', this.props.params.user.id()).then(
+            results => {
+                this.notes = [];
+                this.parseResults(results);
+            },
+            () => {
+                this.loading = false;
+                m.redraw();
+            }
+        )
+    }
+
+    handleOnClickCreate(e) {
+        e.preventDefault();
+        app.modal.show(new NoteCreate(this.props.params));
+    }
 }
