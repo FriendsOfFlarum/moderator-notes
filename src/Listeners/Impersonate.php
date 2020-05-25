edfd9a8dfc884a7af67ff24f34c8e0ab4c1c03fe
Ian Morland <ianm@giffgaff.co.uk>
2020-05-25 10:46:23 +0100

    Support FoF/Impersonate 'reason'

--- src/Listeners/Impersonate.php
+++ src/Listeners/Impersonate.php
@@ -36,14 +36,19 @@ class Impersonate
             new CreateModeratorNote(
                 $event->actor,
                 $event->user->id,
-                app('translator')->trans('fof-moderator-notes.api.auto-note')
+                (property_exists($event, 'switchReason') ? $event->switchReason : app('translator')->trans('fof-moderator-notes.api.auto-note'))
             )
         );
         $this->bus->dispatch(
             new CreateModeratorNote(
                 $event->actor,
                 $event->actor->id,
-                app('translator')->trans('fof-moderator-notes.api.auto-note-actor', ['username' => $event->user->username, 'userId' => $event->user->id])
+                app('translator')->trans('fof-moderator-notes.api.auto-note-actor', [
+                    'username' => $event->user->username,
+                    'userId' => $event->user->id,
+                    'reason' => (property_exists($event, 'switchReason') ? $event->switchReason : app('translator')->trans('fof-moderator-notes.api.no_reason_provided'))
+                    ]
+                )
             )
         );
     }
