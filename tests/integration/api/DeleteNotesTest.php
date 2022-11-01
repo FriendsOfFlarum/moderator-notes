c57aec1793cf1b226b2be215c5f6ecb52c5be886
Ian Morland <ian@morland.me>
2022-11-01 12:43:41 +0000

    basic delete note tests

---
+++ tests/integration/api/DeleteNotesTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of fof/moderator-notes.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\ModeratorNotes\Tests\integration;
+
+use Carbon\Carbon;
+use Flarum\Group\Group;
+use Flarum\Testing\integration\RetrievesAuthorizedUsers;
+use Flarum\Testing\integration\TestCase;
+
+class DeleteNotesTest extends TestCase
+{
+    use RetrievesAuthorizedUsers;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('fof-moderator-notes');
+
+        $this->prepareDatabase([
+            'users' => [
+                ['id' => 3, 'username' => 'a_moderator', 'email' => 'a_mod@machine.local', 'is_email_confirmed' => 1],
+                ['id' => 4, 'username' => 'toby', 'email' => 'toby@machine.local', 'is_email_confirmed' => 1],
+                ['id' => 5, 'username' => 'bad_user', 'email' => 'bad_user@machine.local', 'is_email_confirmed' => 1],
+            ],
+            'group_user' => [
+                ['user_id' => 3, 'group_id' => Group::MODERATOR_ID],
+            ],
+            'users_notes' => [
+                ['id' => 6, 'user_id' => 5, 'note' => '<t><p>bad_user has been naughty</p></t>', 'added_by_user_id' => 3, 'created_at' => Carbon::now()],
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_without_permission_cannot_delete_note()
+    {
+        $response = $this->send(
+            $this->request('DELETE', '/api/moderatorNotes/6', [
+                'authenticatedAs' => 3,
+            ])
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function user_with_permission_can_create_note()
+    {
+        $this->prepareDatabase([
+            'group_permission' => [
+                ['group_id' => Group::MEMBER_ID, 'permission' => 'user.deleteModeratorNotes'],
+            ],
+        ]);
+        
+        $response = $this->send(
+            $this->request('DELETE', '/api/moderatorNotes/6', [
+                'authenticatedAs' => 3,
+            ])
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+}
