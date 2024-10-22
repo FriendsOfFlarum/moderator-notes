e6e936a97d87dd37e716f3a17e747c288e3d7380
IanM <ian@morland.me>
2024-10-22 20:07:26 +0100

    chore: export components, model

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,6 +3,9 @@ import addModeratorNotesPage from './addModeratorNotesPage';
 
 export { default as extend } from './extend';
 
+export * from './components';
+export * from './model';
+
 app.initializers.add('fof-moderator-notes', () => {
   addModeratorNotesPage();
 });
